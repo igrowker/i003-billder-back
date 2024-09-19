0ef9d4fbc769519ad9351f0000fe70e56e1440d7
enzosamojedny <enzosamojedny@gmail.com>
2024-09-18 21:22:09 -0300

    cambiado namespace

--- Billder.Infrastructure/Data/AppDbContext.cs
+++ Billder.Infrastructure/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Billder.Infrastructure.Data
+namespace Billder.Data
 {
     public partial class AppDbContext : DbContext
     {
